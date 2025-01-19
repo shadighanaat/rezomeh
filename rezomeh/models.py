0560ce981cad9bf2757f4be39e53fd7fd46be911
shadighanaat/foroshgah <shadighanaati@gmail.com>
2025-01-19 22:56:15 +0330

    add classes to models

--- rezomeh/models.py
+++ rezomeh/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from audioop import reverse
+
+class AboutMe(models.Model):
+    title = models.CharField(max_length=155)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Servise(models.Model):
+    title = models.CharField(max_length=155)
+    short_description = models.TextField()  
+
+    def __str__(self):
+        return self.title
+
+class Rezomeh(models.Model):
+    title = models.CharField(max_length=155)
+    short_description = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} on {self.datetime_created}"
+
+class Category(models.Model):
+    title = models.CharField(max_length=155)
+
+    def __str__(self):
+        return self.title
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=155)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='protfolios')
+    description = models.TextField()
+    technology = models.CharField(max_length=500) 
+    image = models.ImageField(upload_to='images/',  blank=True)
+    video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('portfolio_detail', args=[self.pk])
+    
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    technology = models.CharField(max_length=100) 
+    description = models.TextField()
+    short_description = models.TextField(blank=True)  
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', args=[self.id])
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100) 
+    message = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Comment author'
+    )
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments', blank=True, null=True) 
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.datetime_created}"
+    
+class Contact(models.Model):
+    fullname = models.CharField(max_length=155)    
+    message = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField(max_length=155)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Contact from {self.fullname}"

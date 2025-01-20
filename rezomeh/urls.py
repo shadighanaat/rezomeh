541f5e6d28cd175f44109792ab4be484ac371d61
shadighanaat/foroshgah <shadighanaati@gmail.com>
2025-01-20 20:55:23 +0330

    making urls for rezomeh

---
+++ rezomeh/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+
+from .views import (AboutMeView, 
+                    BlogDetailView, 
+                    BlogView, 
+                    CommentCreateView, 
+                    ContactView, 
+                    HomeView, 
+                    PortfolioDetailView, 
+                    PortfolioView, 
+                    RezomehView
+                    )
+
+urlpatterns = [
+    path('home/', HomeView.as_view(), name='home_list'),
+    path('aboutme/', AboutMeView.as_view(), name='aboutme_list'),
+    path('rezomeh/', RezomehView.as_view(), name='rezomeh_list'),
+    path('portfolio/', PortfolioView.as_view(), name='portfolio_list'),
+    path('<int:pk>/', PortfolioDetailView.as_view(), name='portfolio_detail'),
+    path('blog/', BlogView.as_view(), name='blog_list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
+    path('contact/', ContactView.as_view(), name='contact_list'),
+    ]

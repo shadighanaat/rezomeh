603148d672c7ed8f732972bcbd076218ebc7818c
shadighanaat/foroshgah <shadighanaati@gmail.com>
2025-01-25 19:13:17 +0330

    add static and rosetta be url

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rezomeh/', include('rezomeh.urls')),
-]
+
+    
+    # rosseta (i18)
+    path('rosetta/', include('rosetta.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

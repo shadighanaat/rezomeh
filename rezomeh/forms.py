f8cd6e01d3266bb85b041b03dc6b730989298c97
shadighanaat <shadighanaati@gmail.com>
2025-04-07 09:22:14 +0330

    Add translation support for Comment and Contact form fields using gettext_lazy

--- rezomeh/forms.py
+++ rezomeh/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import Comment, Contact
+from django.utils.translation import gettext_lazy as _
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(label=_('name'))
+    message = forms.CharField(label=_('message'))
+
     class Meta:
         model = Comment
         fields = ['name', 'message']
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['fullname', 'email', 'phone_number', 'message']        
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, label=_("Name"))
+    email = forms.EmailField(required=True, label=_("Email"))
+    message = forms.CharField(widget=forms.Textarea, required=True, label=_("Message"))

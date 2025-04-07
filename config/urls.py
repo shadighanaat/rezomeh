1f26b850accf27880a1868c05b7978ad292ed0a9
shadighanaat <shadighanaati@gmail.com>
2025-04-07 09:12:39 +0330

    🔧 Customize admin URL using DJANGO_ADMIN_URL environment variable

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
+admin_url = os.environ.get('DJANGO_ADMIN_URL', 'admin') 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{admin_url}/', admin.site.urls),
     path('rezomeh/', include('rezomeh.urls')),
 
     

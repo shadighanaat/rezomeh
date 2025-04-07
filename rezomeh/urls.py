82885d8f9b28090346965e06437b522c9fdafbc5
shadighanaat <shadighanaati@gmail.com>
2025-04-07 09:30:46 +0330

    Add RezomehView, contact_view, switch_language, and download_cv to urlpatterns and set handler500

--- rezomeh/urls.py
+++ rezomeh/urls.py
@@ -3,24 +3,29 @@ from django.urls import path
 from .views import (AboutMeView, 
                     BlogDetailView, 
                     BlogView, 
-                    CommentCreateView, 
-                    ContactView, 
+                    CommentCreateView,  
                     HomeView, 
                     PortfolioDetailView, 
                     PortfolioView, 
-                    RezomehView, switch_language
+                    RezomehView,
+                    contact_view,
+                    switch_language,
+                    download_cv,
+                   
                     )
+handler500 = "your_app.views.custom_server_error"
 
-urlpatterns = [
+urlpatterns =  [
     path('', HomeView.as_view(), name='home'),
     path('home/', HomeView.as_view(), name='home_list'),
     path('aboutme/', AboutMeView.as_view(), name='aboutme_list'),
-    path('rezomeh/', RezomehView.as_view(), name='rezomeh_list'),
+    path('rezome/', RezomehView.as_view(), name='rezomeh_list'),
     path('portfolio/', PortfolioView.as_view(), name='portfolio_list'),
-    path('<int:pk>/', PortfolioDetailView.as_view(), name='portfolio_detail'),
+    path('portfolio/<int:pk>/', PortfolioDetailView.as_view(), name='portfolio_detail'),
     path('blog/', BlogView.as_view(), name='blog_list'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
-    path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
-    path('contact/', ContactView.as_view(), name='contact_list'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/<int:blog_id>/comment', CommentCreateView.as_view(), name='comment_create'),
+    path("contact/", contact_view, name="contact_list"),
     path('language/', switch_language, name='language'),
+    path('download-cv/', download_cv, name='download_cv'),
     ]

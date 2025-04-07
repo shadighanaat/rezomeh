59d89bc251e63d8d3dbbeb5b0db4890c82afc150
shadighanaat <shadighanaati@gmail.com>
2025-04-07 09:14:37 +0330

    ✨ Add title, category, and github_link_django fields to resume model

--- rezomeh/models.py
+++ rezomeh/models.py
@@ -57,12 +57,19 @@ class Category(models.Model):
         return self.title
 
 class Portfolio(models.Model):
+    CATEGORY_CHOICES = [
+        ('personal', 'Personal'),
+        ('store', 'Store'),
+        ('python', 'Python'),
+    ]
     title = models.CharField(max_length=155)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='protfolios')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    slug = slug = models.SlugField()
     description = models.TextField()
     technology = models.CharField(max_length=500) 
     image = models.ImageField(upload_to='images/',  blank=True)
     video_file = models.FileField(upload_to='videos/', blank=True, null=True)
+    github_link_django = models.URLField(max_length=200, blank=True, null=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now_add=True)
 
@@ -71,7 +78,7 @@ class Portfolio(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse('portfolio_detail', args=[self.pk])
+         return reverse('portfolio_detail', kwargs={'pk': self.pk})
     
 class Blog(models.Model):
     title = models.CharField(max_length=100)
@@ -107,6 +114,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.name} on {self.datetime_created}"
+
+    def get_absolute_url(self):
+        return reverse('blog_detail', args=[self.blog.id])    
     
 class Contact(models.Model):
     fullname = models.CharField(max_length=155)    

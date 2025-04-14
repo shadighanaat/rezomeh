9370323ba36214b628b67142246ae8b520932b4a
shadighanaat <shadighanaati@gmail.com>
2025-04-14 23:57:24 +0330

    Add parent_id field to comment form for reply functionality

--- rezomeh/forms.py
+++ rezomeh/forms.py
@@ -5,12 +5,15 @@ from django.utils.translation import gettext_lazy as _
 class CommentForm(forms.ModelForm):
     name = forms.CharField(label=_('name'))
     message = forms.CharField(label=_('message'))
+    parent_id = forms.IntegerField(widget=forms.HiddenInput(), required=False)
+
 
     class Meta:
         model = Comment
-        fields = ['name', 'message']
+        fields = ['name', 'message', 'parent_id']
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100, required=True, label=_("Name"))
     email = forms.EmailField(required=True, label=_("Email"))
     message = forms.CharField(widget=forms.Textarea, required=True, label=_("Message"))
+    
